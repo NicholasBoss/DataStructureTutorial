2bfdf3b5f1bf42f21e984468eb8cb3350fec29cc
Nicholas Boss <NicholasBoss0914@gmail.com>
2022-07-13 16:26:30 -0600

    fixed tree solution

--- treesolution.py
+++ treesolution.py
@@ -6,7 +6,7 @@ class BST:
             self.data = data
             self.left = None
             self.right = None
-
+         
     def __init__(self): #initialize the tree
         self.root = None
 
@@ -14,32 +14,32 @@ class BST:
         if self.root is None:
             self.root = BST.Node(data)
         else:
-            self._insert(data, self.root)  
+            self._insert(data, self.root)
 
+
+    #edit the _insert function to not accept duplicates
     def _insert(self, data, node): #insert a node into the tree
         if data < node.data: #if the data is less than the node data, go left
-            if node.left is None:
-                node.left = BST.Node(data)
-            else:
-                self._insert(data, node.left)
-        elif data > node.data: #if the data is greater than the node data, go right
-            if node.right is None:
-                node.right = BST.Node(data)
-            else:
+            if node.left is None: 
+                node.left = BST.Node(data) 
+            else: 
+                self._insert(data, node.left) 
+        else: #if the data is greater than the node data, go right
+            if node.right is None: 
+                node.right = BST.Node(data) 
+            else: 
                 self._insert(data, node.right) 
-        else: #if the data is equal to the node data, do nothing
-            print("Value already in tree!")
-            pass
+
+
 
     def __iter__(self): #iterate through the tree
-        yield from self._traverse_forward(self.root)  # Start at the root
-        
-    def _traverse_forward(self, node): #iterate through the tree
-
+        yield from self._print_ordered(self.root)  # Start at the root
+    
+    def _print_ordered(self, node): #iterate through the tree
         if node is not None: #if the node is not empty
-            yield from self._traverse_forward(node.left) #go left
+            yield from self._print_ordered(node.left) #go left
             yield node.data #print the node data
-            yield from self._traverse_forward(node.right) #go right
+            yield from self._print_ordered(node.right) #go right
 
 #test case 1
 print("Test case 1:")
